7298748cd462bfcf82f8321cb38086955e7ae833
isqua <isqua@isqua.ru>
2016-10-22 22:54:58 +0300

    Move decoding key to deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,42 +5,39 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into out/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+echo "Clone gh-pages to build"
+
 git clone $REPO build
 pushd build
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 popd
 
-# Clean out existing contents
-rm -rf build/**/* || exit 0
+echo "Clean build"
+rm -rf build/* || exit 0
+ls -l build
 
-# Run our compile script
+echo "Run build"
 npm run build
 
-# Now let's go have some fun with the cloned repo
+echo "Commit"
 cd build
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
-
-# Commit the "changes", i.e. the new version.
-# The delta will show diffs between new and old versions.
-git add .
+git add --all .
 git commit -m ":up: Deploy ${SHA}"
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+echo "Set up git"
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-eval `ssh-agent -s`
-ssh-add .travis/deploy_key
 
-# Now that we're all set up, we can push.
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../.travis/deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+echo "Push changes"
 git push $SSH_REPO $TARGET_BRANCH
